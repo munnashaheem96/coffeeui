a394f0530cc1fd02a885ddce2efe3df202d24f85
Munna Shaheem <munnashaheemperinchikkal@gmail.com>
2022-11-06 20:11:52 +0530

    day 1

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,96 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        leading: Icon(Icons.menu),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right:25.0),
+            child: Icon(Icons.person),
+          )
+          ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: 
+          Icon(Icons.home),
+          label: '',
+          ),
+          BottomNavigationBarItem(icon: 
+          Icon(Icons.favorite),
+          label: '',
+          ),
+          BottomNavigationBarItem(icon: 
+          Icon(Icons.notifications),
+          label: '',
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          //find the best coffee for you
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Text("Find the best coffee for you",
+            style: GoogleFonts.bebasNeue(
+              fontSize: 56,
+            )
+            ),
+          ),
+          SizedBox(height: 25.0),
+          //searchbar
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: TextField(
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.search),
+                hintText: 'Find your coffee..+',
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey.shade600),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey.shade600)
+                ),
+              ),
+            ),
+          ),
+
+          SizedBox(height: 25.0),
+          
+          //horizontal list view of coffe tile
+          Expanded(
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: 200,
+                    color: Colors.grey,
+                  ),
+                ),
+
+          ],
+          ),
+          ),
+        ],
+      ),
+    );
+  }
+}
